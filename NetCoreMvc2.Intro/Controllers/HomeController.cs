77404852e49977866c6e04e80b96c4ecb62c1782
gozcan <gozcan@lstyazilim.com>
2018-12-12 00:51:13 +0300

    First commit

---
+++ NetCoreMvc2.Intro/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreMvc2.Intro.Entities;
+
+namespace NetCoreMvc2.Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello MVC Core";
+        }
+
+        public ViewResult Index2()
+        {
+            return View();
+        }
+
+        public ViewResult Index3()
+        {
+            List<Employee> employees = new List<Employee> {
+                new Employee{Id=1,FirstName="Gorkem",LastName="Ozcan",CityId=34},
+                new Employee{Id=2,FirstName="Faruk",LastName="Ozcan",CityId=41},
+                new Employee{Id=3,FirstName="Aynur",LastName="Ozcan",CityId=41}
+            };
+            return View(employees);
+        }
+    }
+}
